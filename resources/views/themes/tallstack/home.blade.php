96456ebf543649b4e3c77ae344af51b842ca736b
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2021-07-21 19:09:29 +0530

    Test-01 on Theme Options for Home Page

--- resources/views/themes/tallstack/home.blade.php
+++ resources/views/themes/tallstack/home.blade.php
@@ -8,14 +8,14 @@
         <div class="px-12 mx-auto max-w-7xl">
             <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
                 <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
-                    <span class="">Start</span> <span class="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-indigo-600 to-purple-500 lg:inline">building a buzz</span> <span>around your product 🚀</span>
+                    <span class="">Start</span> <span class="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-indigo-600 to-purple-500 lg:inline">building a community</span> <span>with your knowledge 🚀</span>
                 </h1>
                 <p class="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
-                    Start gaining the traction you've always wanted with our next-level templates and designs. Crafted to help you tell your story.
+                theme('home_description')
                 </p>
                 <div class="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                    <a href="#_" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-indigo-600 sm:w-auto sm:mb-0 rounded-xl">
-                        Get Started
+                    <a href="theme('home_cta_url')" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-indigo-600 sm:w-auto sm:mb-0 rounded-xl">
+                        theme('home_cta')
                         <svg class="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </a>
                     <a href="#_" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 sm:w-auto sm:mb-0 rounded-xl">
